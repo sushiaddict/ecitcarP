5452be90d1f14d5e1dce0013d68028def4519de4
Bryce Hongrui Wang <theviolinsomewhere@gmail.com>
2018-05-28 12:30:29 -0700

    Added Bigger Number Game to GIT

---
+++ BiggerNumberGame/app/src/main/java/com/wang/brycewang/biggernumbergame/BiggerNumberGame.java
@@ -0,0 +1,13 @@
+package com.wang.brycewang.biggernumbergame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BiggerNumberGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bigger_number_game);
+    }
+}
